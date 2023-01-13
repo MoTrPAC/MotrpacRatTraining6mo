684cb22d0b1d346f93357f815bfaf49f998a3cce
Nicole Gay <nicolerg@stanford.edu>
2023-01-12 17:53:15 -0800

    address #43 and https://github.com/MoTrPAC/MotrpacRatTraining6moData/issues/41

--- R/normalize_data.R
+++ R/normalize_data.R
@@ -6,6 +6,8 @@
 #' @param min_cpm double, retain genes with more than \code{min_cpm} counts per million in at least \code{min_num_samples} samples
 #' @param min_num_samples double, retain genes with more than \code{min_cpm} counts per million in at least \code{min_num_samples} samples
 #' @param norm_method character, one of \code{c("TMM","TMMwsp","RLE","upperquartile","none")}. "TMM" by default.
+#' @param counts optional user-supplied numeric data frame or matrix where row names are 
+#'   gene IDs and column names are sample identifiers
 #'
 #' @return data frame where row names are feature_ID and column names are viallabel 
 #' @export
@@ -17,11 +19,24 @@
 #' @examples
 #' norm_data = transcript_normalize_counts("LUNG")
 #' 
-transcript_normalize_counts = function(tissue, min_cpm = 0.5, min_num_samples = 2, norm_method="TMM"){
+#' @details 
+#' Note that while this function is identical to the code used to generate the 
+#' normalized RNA-seq data tables ([MotrpacRatTraining6moData::TRNSCRPT_NORM_DATA])
+#' and the normalized RNA-seq data available through the MoTrPAC Data Hub,
+#' \code{transcript_normalize_counts(tissue)} yields slightly fewer genes than its
+#' corresponding [MotrpacRatTraining6moData::TRNSCRPT_NORM_DATA] object. 
+#' Investigation of this discrepancy suggests minor functional differences in the 
+#' version of [edgeR::cpm()] used ~2.5 years apart. Find more details in 
+#' [this GitHub issue](https://github.com/MoTrPAC/MotrpacRatTraining6moData/issues/41).  
+#' 
+transcript_normalize_counts = function(tissue, min_cpm = 0.5, min_num_samples = 2, norm_method = "TMM", counts = NULL){
 
-  counts = fetch_object(sprintf("TRNSCRPT_%s_RAW_COUNTS", gsub("-","",tissue)))
-  rownames(counts) = counts$feature_ID
-  counts[,c("feature","feature_ID","tissue","assay")] = NULL
+  if(!is.null(counts)){
+    counts = as.data.frame(counts)
+  }else{
+    counts = fetch_object(sprintf("TRNSCRPT_%s_RAW_COUNTS", gsub("-","",tissue)))
+    counts = df_to_numeric(counts)
+  }
   
   raw_dge = edgeR::DGEList(counts=counts) 
   keep = rowSums(edgeR::cpm(raw_dge) > min_cpm) >= min_num_samples
@@ -38,13 +53,16 @@ transcript_normalize_counts = function(tissue, min_cpm = 0.5, min_num_samples = 
 #' Filter and normalize raw ATAC-seq counts
 #' 
 #' Download raw counts from Google Cloud Storage and return the filtered and 
-#' quantile-normalized data. 
+#' quantile-normalized data. Alternatively, the user can provide a numeric
+#' data frame of raw ATAC-seq counts. 
 #'
 #' @param tissue `r tissue()`
 #' @param scratchdir character, local directory in which to download data from 
 #'   Google Cloud Storage. Current working directory by default. 
 #' @param n_samples integer, retain features with at least \code{min_count} counts in at least \code{n_samples} samples 
 #' @param min_count integer, retain features with at least \code{min_count} counts in at least \code{n_samples} samples 
+#' @param counts optional user-supplied numeric data frame or matrix where row names are 
+#'   feature IDs and column names are sample identifiers 
 #'
 #' @return data frame where row names are feature_ID and column names are viallabel 
 #' @export
@@ -56,17 +74,26 @@ transcript_normalize_counts = function(tissue, min_cpm = 0.5, min_num_samples = 
 #' \dontrun{
 #' norm_data = atac_normalize_counts("BAT", scratchdir = "/tmp")
 #' }
-atac_normalize_counts = function(tissue, scratchdir = ".", n_samples = 4, min_count = 10){
+#' 
+#' @details 
+#' Non-autosomal peaks are removed, i.e., peak IDs that don't begin with "chrX", "chrY",
+#' or "chr"+number. If you are providing your own counts matrix, ensure that peak IDs
+#' follow the same naming convention. 
+#' 
+atac_normalize_counts = function(tissue, scratchdir = ".", n_samples = 4, min_count = 10, counts = NULL){
   
-  # load raw counts 
-  counts = load_sample_data(tissue = tissue, 
-                            assay = "ATAC", 
-                            normalized = FALSE, 
-                            training_regulated_only = FALSE, 
-                            scratchdir = scratchdir,
-                            exclude_outliers = FALSE)
-  rownames(counts) = counts$feature_ID
-  counts[,c("feature","feature_ID","tissue","assay")] = NULL
+  if(!is.null(counts)){
+    counts = as.data.frame(counts)
+  }else{
+    # load raw counts 
+    counts = load_sample_data(tissue = tissue, 
+                              assay = "ATAC", 
+                              normalized = FALSE, 
+                              training_regulated_only = FALSE, 
+                              scratchdir = scratchdir,
+                              exclude_outliers = FALSE)
+    counts = df_to_numeric(counts)
+  }
   
   # remove non-auto peaks
   counts = counts[grepl("^chr[0-9]|^chrY|^chrX", rownames(counts)),]
